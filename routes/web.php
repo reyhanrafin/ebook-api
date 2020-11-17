bf0a77248a185a2f68c2de4f7b9cdb0faf4ec412
triandikaabriansyah <triandikaabriansyah@gmail.com>
2020-11-17 23:16:03 +0700

    add endpoint/me

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,9 @@
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+ return "XII RPL 4"; 
 });
+
+Route::get('/me', 'AuthController@me'); 
